051775d197315197bfb3b8e690ed05de3cbcf9d5
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-12 16:03:56 +0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("App", () => {
+  test("renders application successfully", () => {
+    render(<App />);
+
+    expect(document.body).toBeInTheDocument();
+  });
+
+  test("renders without crashing", () => {
+    const { container } = render(<App />);
+
+    expect(container).toBeTruthy();
+  });
 });
